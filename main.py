f9498097329db957b0622b1c305839e949157a73
Weronika Zakrzewska <weronikazakrzewska@192.168.1.122>
2024-10-30 21:15:54 +0100

    data import

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-# Z podanego zbioru danych wyselekcjonuj 5 o największej wartości na jednostkę, znając kategorię obiektu
-# Dane znajdują się w folderze "dane" w pliku "zbiór_wejściowy.json" oraz "kategorie.json"
-# Wynik przedstaw w czytelnej formie na standardowym wyjściu
+import json
+
+with open("kategorie.json") as kat:
+    kat = json.loads(kat.read())
+
+with open("zbiór_wejściowy.json") as data:
+    data = json.loads(data.read())
+
+print(type(data[2]))
+
