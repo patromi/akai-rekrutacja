42de74d11174c541e19fdf82ffafb0d97a826c65
Weronika Zakrzewska <weronikazakrzewska@192.168.1.122>
2024-10-30 22:03:40 +0100

    mass to ounces

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import json
 
-with open("kategorie.json") as kat:
-    kat = json.loads(kat.read())
+with open("kategorie.json") as cat:
+    cat = json.loads(cat.read())
 
 with open("zbiór_wejściowy.json") as data:
     data = json.loads(data.read())
 
-print(type(data[2]))
+# zamiana masy na uncje
 
+for i in range(len(data)):
+    masa = data[i]["Masa"]
+    masa = masa.replace(",", ".")
+    if masa[-1] == "t":
+        masa = float(masa[:-2])/141.7
+    else:
+        masa = float(masa[:-1])/28.35
+    print(masa)
