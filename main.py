8aeeef33d999d8059367eafe6b9e02cf4c293288
Weronika Zakrzewska <weronikazakrzewska@192.168.1.122>
2024-10-31 11:30:45 +0100

    price

--- main.py
+++ main.py
@@ -12,7 +12,16 @@ for i in range(len(data)):
     masa = data[i]["Masa"]
     masa = masa.replace(",", ".")
     if masa[-1] == "t":
-        masa = float(masa[:-2])/141.7
+        data[i]["Masa"] = float(masa[:-2])/141.7
     else:
-        masa = float(masa[:-1])/28.35
-    print(masa)
+        data[i]["Masa"] = float(masa[:-1])/28.35
+
+# przypisanie ceny
+for i in range(len(data)):
+    data[i].update({"Cena": 0})
+    for j in range(len(cat)):
+        if data[i]["Typ"] == cat[j]["Typ"] and data[i]["Czystość"] == cat[j]["Czystość"]:
+            data[i]["Cena"] = data[i]["Masa"]*cat[j]["Wartość za uncję (USD)"]
+            break
+
+print(data)
